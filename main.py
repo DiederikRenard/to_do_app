7de1668c1b4240da57916b01a383cdedf488800a
DiederikRenard <diederik.h.renardel@gmail.com>
2023-10-05 13:15:58 +0200

    replace json with SQL and work out delete function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import messagebox
-
+import sqlite3
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 import json
@@ -16,6 +16,13 @@ import json
 # TODO create suggest function to search for to-dos
 
 # TODO create delete function to erase to-dos that are to-done
+
+connection = sqlite3.connect("to-dos.db")
+cursor = connection.cursor()
+
+# cursor.execute("CREATE TABLE to_dos(title, duration, description)")
+res = cursor.execute("SELECT name FROM sqlite_master")
+res.fetchone()
 
 
 class ToDoApp:
@@ -60,8 +67,6 @@ class ToDoApp:
         self.b1 = ttk.Button(text="Submit", bootstyle="success", command=self.add_to_do)
         self.b1.grid(column=0, row=7, padx=10, pady=5)
 
-        # Search for To-Do
-
         self.search_b2 = ttk.Button(text="Got time?", bootstyle="warning-outline", command=self.got_time)
         self.search_b2.grid(column=0, row=8, padx=10, pady=5)
 
@@ -70,9 +75,16 @@ class ToDoApp:
 
         self.delete_b3 = ttk.Button(text="Delete", bootstyle="danger-outline", command=self.delete)
         self.delete_b3.grid(column=1, row=8, padx=10, pady=5)
-        self.row_number = 0
-        self.list_todos()
+        label_row = 0
 
+        for row in cursor.execute("SELECT title, duration FROM to_dos ORDER BY duration"):
+            print(row)
+            self.to_do_label_title = ttk.Label(text=f"")
+            self.to_do_label_time = ttk.Label(text=f"")
+            self.to_do_label_title.grid(column=3, row=label_row, padx=10, pady=5)
+            self.to_do_label_time.grid(column=4, row=label_row, padx=10, pady=5)
+            label_row += 1
+        self.list_todos()
 
 
     def add_to_do(self):
@@ -82,76 +94,60 @@ class ToDoApp:
         minute = int(self.min_scroll.get())
         time = f"{hour}.{minute}"
         text = self.description_text.get("1.0", END)
-        new_to_do = {
-            float(time): {
-                "what": todo_name,
-                "message": text,
-            }
-        }
-        try:
-            with open("todo.json", "r") as data_file:
-                data = json.load(data_file)
-        except FileNotFoundError:
-            with open("todo.json", "w") as data_file:
-                json.dump(new_to_do, data_file, indent=4)
-        else:
-            data.update(new_to_do)
-            with open("todo.json", "w") as data_file:
-                json.dump(data, data_file, indent=4)
-        finally:
-            self.to_do_text.delete(0, END)
-            self.hour_scroll.set(0)
-            self.min_scroll.set(00)
-            self.description_text.delete("1.0", END)
-            self.list_todos()
+        to_add = (f"{todo_name}", time, str(text))
+        cursor.execute("INSERT INTO to_dos VALUES(?, ?, ?)", to_add)
+        connection.commit()
+        self.to_do_text.delete(0, END)
+        self.hour_scroll.set(0)
+        self.min_scroll.set(00)
+        self.description_text.delete("1.0", END)
+        self.list_todos()
 
     def got_time(self):
         search_hour = int(self.hour_scroll.get())
         search_min = int(self.min_scroll.get())
         search_time = search_hour + (search_min / 100)
-        try:
-            with open("todo.json", "r") as data_file:
-                data = json.load(data_file)
-                for item in data:
-                    if float(search_time) >= float(item):
-                        messagebox.showinfo(title=f"{data[item]['what']}",
-                                            message=f"{data[item]['message']}\n"
-                                                    f"Should take about {search_hour}h{search_min}m")
-                        break
-        except FileNotFoundError:
-            messagebox.showwarning("File not found. No To-dos have been made yet!")
-        finally:
-            self.hour_scroll.set(0)
-            self.min_scroll.set(00)
-            self.list_todos()
+        all_times = []
+        for row in cursor.execute("SELECT duration, title, description FROM to_dos ORDER BY duration DESC"):
+            duration = float(row[0])
+            all_times.append(duration)
+            if float(row[0]) <= search_time:
+                messagebox.showinfo(title=row[1], message=f"You can {row[2]}.\n It should take around {row[0]}h.")
+                break
+        if float(all_times[-1]) > search_time:
+            messagebox.showinfo(title="Found nothing",
+                                message="You do not have enough time for anything on your list")
+
+        self.hour_scroll.set(0)
+        self.min_scroll.set(00)
+        self.list_todos()
 
     def delete(self):
-        to_delete = self.to_do_text.get()
-        try:
-            with open("todo.json", "w") as data_file:
-                data = json.load(data_file)
-                for item in data:
-                    if data[item]['what'] == to_delete:
-                        print(data[item])
-                        # del data.item
-                        print(data)
-                        break
-        except FileNotFoundError:
-            messagebox.showwarning("File not found. No To-dos have been made yet!")
+        to_delete = str(self.to_do_text.get())
+        sql = 'DELETE FROM to_dos WHERE title=?'
+        cur = connection.cursor()
+        cur.execute(sql, (to_delete,))
+        connection.commit()
+
         self.list_todos()
 
     def list_todos(self):
-        try:
-            with open("todo.json", "r") as data_file:
-                data = json.load(data_file)
+        self.to_do_label_title.destroy()
+        self.to_do_label_time.destroy()
+        label_row = 0
+        for row in cursor.execute("SELECT title, duration FROM to_dos ORDER BY duration"):
+            print(row)
 
-                for item in data:
-                    self.to_do_label = ttk.Label(text=f"{data[item]['what']}")
-                    self.to_do_label.grid(column=3, row=self.row_number, columnspan=2, padx=10, pady=5)
-                    self.row_number += 1
-        except FileNotFoundError:
-            pass
+            self.to_do_label_title = ttk.Label(text=f"")
+            self.to_do_label_title.config(text=f"")
+            self.to_do_label_title.config(text=f"{row[0]}")
+            self.to_do_label_title.grid(column=3, row=label_row, padx=10, pady=5)
 
+            self.to_do_label_time = ttk.Label(text=f"")
+            self.to_do_label_time.config(text=f"")
+            self.to_do_label_time.config(text=f"{row[1]}h")
+            self.to_do_label_time.grid(column=4, row=label_row, padx=10, pady=5)
+            label_row += 1
 
 
 if __name__ == "__main__":
